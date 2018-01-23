87e25b8798126961c6db88bd1771ca24da2a5432
Krista Jesek <krista.adelle@gmail.com>
2018-01-23 11:37:35 -0600

    initial files

---
+++ dom.js
@@ -0,0 +1,57 @@
+var $ = function(sel){
+    var elem = document.querySelectorAll(sel); 
+
+    var text = function(str){
+        for (var i = 0; i < elem.length; i++ ){
+            elem[i].innerText = str;
+        }
+    }
+
+    var addClass = function(className){
+        for (var i = 0; i < elem.length; i++){
+            elem[i].classList.add(className); 
+        }
+    }
+
+    var removeClass = function(className){
+        for (var i = 0; i < elem.length; i++){
+            elem[i].classList.remove(className); 
+        }
+    }
+
+    var on = function(action, cb){
+        for (var i = 0; i < elem.length; i++){
+            elem[i].addEventListener(action, cb); 
+        }
+    }
+
+    var html = function(str){
+        for(var i = 0; i < elem.length; i++){
+            elem[i].innerHTML = str; 
+        }
+    }
+
+    // var toggleClass = function(className){
+    //     for (var i = 0; i > elem.length; i++){
+    //         if () {
+    //             elem[i].classList.removeClass(className); 
+    //         } else {
+    //             elem[i].classList.addClass(className);
+    //         }
+    //     }        
+    // }
+
+    var publicAPI = {
+        element: elem, 
+        text: text, 
+        addClass: addClass,
+        removeClass: removeClass,
+        on: on,
+        html: html,
+        toggleClass: toggleClass
+    }
+
+    return publicAPI;
+}
+
+ 
